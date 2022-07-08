864c149bbd6895625a12100390896228a589f1d7
jaiflores <jaianny.vivian@gmail.com>
2022-07-08 16:51:51 -0300

    Primeiro trabalho de inglês

---
+++ script.js
@@ -0,0 +1,77 @@
+var comedy = [
+    {
+        title: "Isn't It Romantic",
+        director: "Todd Strauss-Schulson",
+        year: "2019"
+    },
+    {
+        title: "Pitch Perfect",
+        director: "Jason Moore",
+        year: "2012"
+    },
+    {
+        title: "Anchorman : The Legend of Ron Burgundy",
+        director: "Adam McKay",
+        year: "2004"
+    }
+]
+var action = [
+    {
+        title: "The Matrix",
+        director: "Lana Wachowski, Lilly Wachowski",
+        year: "1999"
+    },
+    {
+        title: "Guardians Of The Galaxy",
+        director: "James Gunn",
+        year: "2014"
+    },
+    {
+        title: "Black Panther",
+        director: "Ryan Coogler",
+        year: "2018"
+    }
+]
+var fantasy = [
+    {
+        title: "Avatar",
+        director: "James Cameron",
+        year: "2009"
+    },
+    {
+        title: "Raya and The Last Dragon",
+        director:"Paul Briggs, Don Hall, John Ripa",
+        year: "2021"
+    },
+    {
+        title: "Franquia Harry Potter",
+        director: "Diversos",
+        year: "2001 - até dias atuais"
+    }
+]
+
+
+if(fantasy = "Franquia Harry Potter" ) {
+        console.log("MY FAVORIT MOVIE IS", "Franquia Harry Potter");
+    }
+    else{
+        console.log("NOT EVEN 5 STARS...");
+    }
+
+
+
+    //Crie 3 variáveis, cada uma com um array:
+// - Comedy
+// -Action
+// -Fantasy 
+
+//Dentro de cada array, insira 3 (TRÊS) Objetos, contendo:
+// -title:
+// -director:
+// -year: 
+
+// *DESAFIO EXTRA* 
+
+//Escolha uma variável e insira uma condição para ela, (condição de sua escolha), com a seguinte mensagem ao console: 
+// IF: "MY FAVORIT MOVIE IS..."
+// ELSE: "NOT EVEN 5 STARS..."
